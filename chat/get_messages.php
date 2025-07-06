af6f4e548dc9cd48c02ba8027a6c537dfb67b0f8
yhqtv <beatriceperry61@gmail.com>
2025-07-06 21:26:35 +0800

    Delete chat directory

--- chat/get_messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (file_exists('messages.txt')) {
-    $messages = file_get_contents('messages.txt');
-    $messages = nl2br($messages); // Convert newlines to <br> for HTML display
-    echo $messages;
-}
-?>
